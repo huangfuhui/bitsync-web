22010e62d658f3ce98e8b722d07de7683ae5ea6c
huangfuhui <huangfuhui@outlook.com>
2018-06-30 01:13:33 +0800

    注册接口对接

--- static/js/bitsync.js
+++ static/js/bitsync.js
@@ -2,4 +2,19 @@
  * 基础js文件
  */
 
-var BASE_RUL = "localhost:8090"
+var BASE_RUL = "http://localhost:8090"
+
+/**
+ * 程序睡眠一段时间
+ * @param numberMillis 毫秒
+ * @returns {boolean}
+ */
+function sleep(numberMillis) {
+    var now = new Date();
+    var exitTime = now.getTime() + numberMillis;
+    while (true) {
+        now = new Date();
+        if (now.getTime() > exitTime)    return true;
+    }
+}
+
